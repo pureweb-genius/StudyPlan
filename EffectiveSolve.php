b94380a984eed847c35387b8798360a28e09b2f6
Nurlybek123456 <baurzannurlybek32@gmail.com>
2024-01-27 05:16:33 +0600

    EffectiveSolve was optimized

--- EffectiveSolve.php
+++ EffectiveSolve.php
@@ -11,17 +11,14 @@ foreach ($studentList as $student) {
     if ($student['actual']) {
         $key = $student['program_id'] . '-' . $student['year_id'] . '-' . $student['spec_id'];
         if (!isset($indexedStudyPlans[$key])) {
-            $result[$key] = [
+            $result[] = [
                 'program_id' => $student['program_id'],
                 'year_id' => $student['year_id'],
                 'spec_id' => $student['spec_id'],
             ];
+            $indexedStudyPlans[$key] = true;
         }
     }
 }
-
-
-$result = array_values($result);
-
 
 print_r($result);
